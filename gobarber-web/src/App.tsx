b562c9d193f11dfa75be9a8610ff896fa92c976a
Mateus Henrique <hi@mateus.cc>
2020-06-04 08:49:39 -0300

    feat: Context API

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -3,11 +3,16 @@ import React from 'react';
 import SignIn from './pages/SignIn/SignIn.layout';
 // import SignUp from './pages/SignUp/SignUp.layout';
 
+import AuthContext from './context/AuthContext';
+
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthContext.Provider value={{ name: 'Mateus' }}>
+      <SignIn />
+    </AuthContext.Provider>
+
     <GlobalStyle />
   </>
 );
