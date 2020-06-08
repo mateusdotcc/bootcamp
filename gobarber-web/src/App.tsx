162f6f1e7f21f2f448af133f9a38599a64453ee5
Mateus Henrique <hi@mateus.cc>
2020-06-08 08:40:29 -0300

    chore: add ContextAPI and toast component

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 import SignIn from './pages/SignIn/SignIn.layout';
 // import SignUp from './pages/SignUp/SignUp.layout';
 
-import AuthContext from './context/AuthContext';
+import AppProvider from './hooks';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <AuthContext.Provider value={{ name: 'Mateus' }}>
+    <AppProvider>
       <SignIn />
-    </AuthContext.Provider>
+    </AppProvider>
 
     <GlobalStyle />
   </>
