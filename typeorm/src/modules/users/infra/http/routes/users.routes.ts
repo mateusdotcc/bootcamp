71b8991668400b8972e0511767f5aa743b880285
Mateus Henrique <hi@mateus.cc>
2020-07-01 18:52:21 -0300

    chore: gobarber DDD

--- typeorm/src/modules/users/infra/http/routes/users.routes.ts
+++ typeorm/src/modules/users/infra/http/routes/users.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
 
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import CreateUserService from '@modules/users/services/CreateUserServices';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
@@ -12,13 +13,18 @@ const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   const { name, email, password } = request.body;
-  const createUser = new CreateUserService();
+
+  const usersRepository = new UsersRepository();
+  const createUser = new CreateUserService(usersRepository);
+
   const user = await createUser.axecute({
     name,
     email,
     password,
   });
+
   delete user.password;
+
   return response.json(user);
 });
 
@@ -27,12 +33,16 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    const updateUserAvatar = new UpdateUserAvatarService();
+    const usersRepository = new UsersRepository();
+    const updateUserAvatar = new UpdateUserAvatarService(usersRepository);
+
     const user = await updateUserAvatar.execute({
       user_id: request.user.id,
       avatarFilename: request.file.filename,
     });
+
     delete user.password;
+
     return response.json(user);
   },
 );
